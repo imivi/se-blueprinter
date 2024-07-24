3156f36f384d7f810d06fe4764b93e2066455787
VM <contact@imivi.dev>
2024-07-24 23:08:19 +0200

    Updates

--- assets/CreateBlueprintButtons-B9N8yqU9.js
+++ assets/CreateBlueprintButtons-B9N8yqU9.js
@@ -1,4 +1,4 @@
-import{a as kt,g as Ht,r as Ct,j as dt}from"./index-CjoDv1BY.js";import{c as Lt,a as wt,F as Ot,g as Dt,b as Gt,u as Ft,d as Nt,e as Mt,I as Ut}from"./Scene-C2IpDMUP.js";/**
+import{a as kt,g as Ht,r as Ct,j as dt}from"./index-BGkTNnK6.js";import{c as Lt,a as wt,F as Ot,g as Dt,b as Gt,u as Ft,d as Nt,e as Mt,I as Ut}from"./Scene-B4-GrlXB.js";/**
  * @license @tabler/icons-react v3.9.0 - MIT
  *
  * This source code is licensed under the MIT license.
